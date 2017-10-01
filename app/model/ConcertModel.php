07512e275fe125b3b19c09913e7620bdbf36bdb1
Krystof Tulinger <ktulinger@rocketfuel.com>
2017-10-01 23:25:45 +0200

    removing unused member variable

--- app/model/ConcertModel.php
+++ app/model/ConcertModel.php
@@ -9,11 +9,6 @@ use Tulinkry\Model\BaseModel;
 
 class ConcertModel extends BaseModel
 {
-
-    const lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-";
     const ALBUM_ID = "6297830325684432193";
 
     private $concerts;
