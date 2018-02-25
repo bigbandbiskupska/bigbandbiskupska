512b367b9c94b57d6086179e5df045a1a3f51f89
tulinkry <k.tulinger@seznam.cz>
2018-02-25 14:32:21 +0100

    removing unused variable

--- app/model/MemberModel.php
+++ app/model/MemberModel.php
@@ -10,14 +10,6 @@ class MemberModel extends BaseModel {
     private $oldmembers;
 
     public function __construct() {
-
-        $lorem = ""; /* "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum."; */
-
         $members = \Nette\Neon\Neon::decode(file_get_contents(__DIR__ . '/models/members.neon'));
 
         $this->sections = array_map(function($o) {
