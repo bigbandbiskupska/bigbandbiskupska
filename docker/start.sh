b7c8068e1bde77ff0a4e15570af5ec8a79bce58d
Krystof Tulinger <k.tulinger@seznam.cz>
2019-06-05 13:31:16 +0200

    using correct composer file

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /app && \
-php composer.phar --no-interaction update && \
+php /usr/bin/composer.phar --no-interaction update && \
 echo "Serving on 0.0.0.0:80" && \
 php -S 0.0.0.0:80 -t www
